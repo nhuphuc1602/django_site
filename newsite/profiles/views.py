a7eb7f7276e75c969862a8cafbcc2e489dc197c0
nhuphuc1602 <69628085+nhuphuc1602@users.noreply.github.com>
2020-08-17 18:07:24 +0700

    update

--- newsite/profiles/views.py
+++ newsite/profiles/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from news.models import Posts
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django import forms
+from django.contrib.auth.models import User
 
 class login_view(LoginView):
     template_name = 'login.html'
@@ -13,5 +16,15 @@ class login_view(LoginView):
 class EditProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'profile.html'
 
-class RegisterView(TemplateView):
+class RegisterForm(UserCreationForm):
+    #email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        field_classes = {'username': UsernameField}
+        
+
+class RegisterView(FormView):
     template_name = 'register.html'
+    form_class = RegisterForm
+
